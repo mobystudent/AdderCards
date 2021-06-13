39fa67e95c8df34970f3902980eae28e49137a2a
mobystudent <mobystudent@gmail.com>
2021-06-13 19:39:54 +0300

    Add assign qr-codes to users

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,6 @@ $(window).on('load', () => {
 	changeCountQRCodes();
 	addIDinDB();
 	sortPerson();
-	createQRCode();
 
 	countItems('#tableTime .table__content', 'time');
 
@@ -352,25 +351,6 @@ function countItems(tableContent, modDepart) {
 	$(`.main__count--${modDepart}`).text(countItemfromDep);
 }
 
-// Закрузка QR-кодов
-function changeCountQRCodes() {
-	countQRCodes('.field__textarea');
-
-	$('.field__textarea').bind('input', (e) => {
-		countQRCodes(e.target);
-	});
-}
-
-function countQRCodes(elem) {
-	const itemCodesContext = $(elem).val();
-	const itemCodes = itemCodesContext.split('\n');
-	const filterItems = itemCodes.filter((item) => item ? true : false);
-	const countQRs = filterItems.length;
-
-	$('.main__count--download').text(countQRs);
-	createQRCode(filterItems);
-}
-
 // Добавление
 function addIDinDB() {
 	$('.btn--add').click((e) => {
@@ -450,12 +430,43 @@ function switchSortButton(arr, depart) {
 	return arr;
 }
 
+// Закрузка QR-кодов
+function changeCountQRCodes() {
+	countQRCodes('.field__textarea');
+	generateQRCode('.field__textarea');
+
+	$('.field__textarea').bind('input', (e) => {
+		countQRCodes(e.target);
+		generateQRCode(e.target);
+	});
+}
+
+function arrayQRCodes(elem) {
+	const itemCodesContext = $(elem).val();
+	const itemCodes = itemCodesContext.split('\n');
+	const filterItems = itemCodes.filter((item) => item ? true : false);
+
+	return filterItems;
+}
+
+function countQRCodes(elem) {
+	const filterItems = arrayQRCodes(elem);
+	const countQRs = filterItems.length;
+
+	$('.main__count--download').text(countQRs);
+}
+
+function generateQRCode(elem) {
+	const filterItems = arrayQRCodes(elem);
+
+	createQRCode(filterItems);
+	assignUserQRCode(filterItems);
+}
+
 function createQRCode(arrCodes) {
 	const canvasArray = $('.canvas__code');
-	// console.log(canvas);
 
 	[...canvasArray].forEach((item, i) => {
-		console.log(typeof(arrCodes[i]));
 		QRCode.toDataURL(arrCodes[i])
 		.then(url => {
 			$(item).attr('src', url);
@@ -464,4 +475,30 @@ function createQRCode(arrCodes) {
 			console.error(err);
 		})
 	});
+}
+
+function assignUserQRCode(arrCodes) {
+	const dataArr = JSON.parse(stringifyJSON());
+	const depart = Object.values(dataArr).map((item) => item);
+	const filterArrQRs = depart.filter((item) => {
+		if (item.StatusID == 'newQR' || item.StatusID == 'changeQR') return item;
+	});
+	const countUserHasCode = filterArrQRs.length;
+	const fieldsQRCodes = $('.table--qr .table__cell--cardid input');
+
+	filterArrQRs.forEach((item, i) => {
+		item.CardID = arrCodes[i];
+		$(fieldsQRCodes[i]).val(arrCodes[i])
+	});
+
+	remoteAddedCodes(countUserHasCode);
+}
+
+function remoteAddedCodes(count) {
+	const filterItems = arrayQRCodes('.field__textarea');
+	filterItems.splice(0, count);
+	const joinnewArrCodes = filterItems.join('\n');
+
+	$('.field__textarea').val(joinnewArrCodes);
+	countQRCodes('.field__textarea');
 }
