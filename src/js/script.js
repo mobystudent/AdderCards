a466e201eccef9df0f81379fb31b06df2b1de858
mobystudent <mobystudent@gmail.com>
2021-06-25 20:48:08 +0300

    Add timecard in class

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 import $ from 'jquery';
 import { json } from './data.js';
 import { shortNameDepart } from './shortNameDepart.js';
+import timeCard from './timecards.js';
 // import Convert from './convert.js';
 import convert from './convert.js';
 import qrcodes from './qrcodes.js';
@@ -14,8 +15,6 @@ $(window).on('load', () => {
 	clearNumberCard();
 	printReport();
 	switchControl();
-	addTimeCard();
-	deleteTimeCard();
 	addIDinDB();
 	sortPerson();
 	permissionAdd();
@@ -27,6 +26,8 @@ $(window).on('load', () => {
 
 	convert.viewConvertCardId();
 	qrcodes.changeCountQRCodes();
+	timeCard.addTimeCard();
+	timeCard.deleteTimeCard();
 });
 
 function getData() {
@@ -77,6 +78,12 @@ function delegationID() {
 		addCountCards(filterArrQRs, '#tableQR', 'qr');
 		focusFirstCell('qr');
 	}
+}
+
+function countItems(tableContent, modDepart) {
+	const countItemfromDep = $(tableContent).eq(0).find('.table__row').length;
+
+	$(`.main__count--${modDepart}`).text(countItemfromDep);
 }
 
 function addTabs(filterIDItems, nameTable, modDepart) {
@@ -329,56 +336,6 @@ function focusFirstCell(nameTable) {
 	const rows = $(`.table--${nameTable} .table__content--active`).find('.table__row');
 
 	$(rows).eq(0).find('.table__input').focus();
-}
-
-// Временные карты
-function addTimeCard() {
-	$('.main__btn').click(() => {
-		$('#tableTime .table__content').append(`
-			<div class="table__row table__row--time">
-				<div class="table__cell table__cell--body table__cell--nametitle">
-					<span class="table__text table__text--body">Временная карта</span>
-				</div>
-				<div class="table__cell table__cell--body table__cell--cardid">
-					<input class="table__input" />
-				</div>
-				<div class="table__cell table__cell--body table__cell--cardname">
-					<span class="table__text table__text--body"></span>
-				</div>
-				<div class="table__cell table__cell--body table__cell--clear">
-					<button class="table__btn table__btn--clear" type="button">
-						<svg class="icon icon--clear">
-							<use class="icon__item" xlink:href="#clear"></use>
-						</svg>
-					</button>
-				</div>
-				<div class="table__cell table__cell--body table__cell--delete">
-					<button class="table__btn table__btn--delete" type="button">
-						<svg class="icon icon--delete">
-							<use class="icon__item" xlink:href="#delete"></use>
-						</svg>
-					</button>
-				</div>
-			</div>
-		`);
-
-		countItems('#tableTime .table__content', 'time');
-		deleteTimeCard();
-	});
-}
-
-function deleteTimeCard() {
-	$('.table__btn--delete').click((e) => {
-		$(e.target).parents('.table__row--time').remove();
-
-		countItems('#tableTime .table__content', 'time');
-	});
-}
-
-function countItems(tableContent, modDepart) {
-	const countItemfromDep = $(tableContent).eq(0).find('.table__row').length;
-
-	$(`.main__count--${modDepart}`).text(countItemfromDep);
 }
 
 // Добавление
