962bf58d28fd4d258d1f9f9e067c8d1be02daa4f
mobystudent <mobystudent@gmail.com>
2021-08-21 20:55:45 +0300

    Delete template folder

--- src/template/includes/favicon.php
+++
@@ -1,2 +0,0 @@
-
-<link rel="favicon" href="favicon/favicon.ico"/>
--- src/template/includes/footer.php
+++
@@ -1,6 +0,0 @@
-
-<footer class="footer">
-  <div class="container">
-    <div class="footer__wrap"><small class="footer__version">v2.2.4</small></div>
-  </div>
-</footer>
